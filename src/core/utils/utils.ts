ce9085fb8a779db7c4d53e9fdb01ec2eb7356a05
Web3MTeam <shanyuzhang123@gmail.com>
2024-04-06 21:37:14 -0400

    add compile script back

--- src/core/utils/utils.ts
+++ src/core/utils/utils.ts
@@ -4,7 +4,7 @@ export async function getLucid() {
   const lucid = await Lucid.new(
     new Blockfrost(
       "https://cardano-preprod.blockfrost.io/api/v0",
-      "preprodMutKHGxUr3JbttdfUaDBlvI9oWGjA75a"
+      process.env.BLOCKFROST_API_KEY
     ),
     "Preprod"
   );
