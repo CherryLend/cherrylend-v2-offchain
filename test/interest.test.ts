3437affb9676ab0d5483df8710ee881d26434efd
Web3MTeam <shanyuzhang123@gmail.com>
2024-05-07 02:03:29 -0400

    finish all edge case tests

--- test/interest.test.ts
+++ test/interest.test.ts
@@ -8,74 +8,147 @@ import {
   AssetClassD,
   InterestConfig,
 } from "../src/index.ts";
+import { describe } from "node:test";
 
 type LucidContext = {
   lucid: Lucid;
-  lender: any;
+  users: any;
   emulator: Emulator;
 };
 
 beforeEach<LucidContext>(async (context) => {
-  const lender = await generateAccountSeedPhrase({
-    lovelace: BigInt(100_000_000),
-  });
-  context.lender = lender;
-  context.emulator = new Emulator([lender]);
+  context.users = {
+    account1: await generateAccountSeedPhrase({
+      lovelace: BigInt(100_000_000),
+    }),
+    account2: await generateAccountSeedPhrase({
+      lovelace: BigInt(100_000_000),
+    }),
+  };
+
+  context.emulator = new Emulator([
+    context.users.account1,
+    context.users.account2,
+  ]);
   context.lucid = await Lucid.new(context.emulator);
 });
 
-test<LucidContext>("Can create interest transaction", async ({
-  lucid,
-  lender,
-}) => {
-  lucid.selectWalletFromSeed(lender.seedPhrase);
+describe("Interest", () => {
+  test<LucidContext>("Can create interest transaction", async ({
+    lucid,
+    users,
+  }) => {
+    lucid.selectWalletFromSeed(users.account1.seedPhrase);
 
-  const { interestScriptAddress } = await getValidators();
+    const { interestScriptAddress } = await getValidators();
 
-  const lenderPubKeyHash = lucid.utils.getAddressDetails(
-    await lucid.wallet.address()
-  ).paymentCredential?.hash;
+    const lenderPubKeyHash = lucid.utils.getAddressDetails(
+      await lucid.wallet.address()
+    ).paymentCredential?.hash;
 
-  const loanAsset: AssetClassD = {
-    policyId: "a1deebd26b685e6799218f60e2cad0a80928c4145d12f1bf49aebab5",
-    tokenName: "4d657368546f6b656e",
-  };
+    const loanAsset: AssetClassD = {
+      policyId: "a1deebd26b685e6799218f60e2cad0a80928c4145d12f1bf49aebab5",
+      tokenName: "4d657368546f6b656e",
+    };
 
-  const interestAsset: AssetClassD = {
-    policyId: "a1deebd26b685e6799218f60e2cad0a80928c4145d12f1bf49aebab5",
-    tokenName: "4d657368546f6b656e",
-  };
+    const interestAsset: AssetClassD = {
+      policyId: "a1deebd26b685e6799218f60e2cad0a80928c4145d12f1bf49aebab5",
+      tokenName: "4d657368546f6b656e",
+    };
 
-  const interestDatum: InterestDatum = {
-    repayLoanAmount: BigInt(100),
-    repayLoanAsset: loanAsset,
-    repayInterestAmount: BigInt(100),
-    repayInterestAsset: interestAsset,
-    lenderPubKeyHash: lenderPubKeyHash as string,
-  };
+    const interestDatum: InterestDatum = {
+      repayLoanAmount: BigInt(100),
+      repayLoanAsset: loanAsset,
+      repayInterestAmount: BigInt(100),
+      repayInterestAsset: interestAsset,
+      lenderPubKeyHash: lenderPubKeyHash as string,
+    };
 
-  const datum = Data.to(interestDatum, InterestDatum);
+    const datum = Data.to(interestDatum, InterestDatum);
 
-  const cancelLoanUTxO: UTxO = {
-    txHash: "009e369a09d92ef324b361668978055d1d707941db2db670d79ea0f6f93a7f67",
-    outputIndex: 1,
-    assets: {
-      lovelace: 1861920n,
-      a1deebd26b685e6799218f60e2cad0a80928c4145d12f1bf49aebab54d657368546f6b656e:
-        100n,
-    },
-    address: interestScriptAddress,
-    datumHash: undefined,
-    datum: datum,
-    scriptRef: undefined,
-  };
+    const cancelLoanUTxO: UTxO = {
+      txHash:
+        "009e369a09d92ef324b361668978055d1d707941db2db670d79ea0f6f93a7f67",
+      outputIndex: 1,
+      assets: {
+        lovelace: 1861920n,
+        a1deebd26b685e6799218f60e2cad0a80928c4145d12f1bf49aebab54d657368546f6b656e:
+          100n,
+      },
+      address: interestScriptAddress,
+      datumHash: undefined,
+      datum: datum,
+      scriptRef: undefined,
+    };
 
-  const interestConfig: InterestConfig = {
-    interestUTxOs: [cancelLoanUTxO],
-    lenderPubKeyHash: lenderPubKeyHash as string,
-  };
+    const interestConfig: InterestConfig = {
+      interestUTxOs: [cancelLoanUTxO],
+      lenderPubKeyHash: lenderPubKeyHash as string,
+    };
 
-  const tx = await interestTx(lucid, interestConfig);
+    const tx = await interestTx(lucid, interestConfig);
 
-  expect(tx.type).toBe("success");
+    expect(tx.type).toBe("success");
+  });
+
+  test<LucidContext>("Can not create interest transaction if not signed by lender", async ({
+    lucid,
+    users,
+  }) => {
+    lucid.selectWalletFromSeed(users.account1.seedPhrase);
+
+    const { interestScriptAddress } = await getValidators();
+
+    const accoun1PubKeyHash = lucid.utils.getAddressDetails(
+      await lucid.wallet.address()
+    ).paymentCredential?.hash;
+
+    const account2PubKeyHash = lucid.utils.getAddressDetails(
+      await users.account2.address
+    ).paymentCredential?.hash;
+
+    const loanAsset: AssetClassD = {
+      policyId: "a1deebd26b685e6799218f60e2cad0a80928c4145d12f1bf49aebab5",
+      tokenName: "4d657368546f6b656e",
+    };
+
+    const interestAsset: AssetClassD = {
+      policyId: "a1deebd26b685e6799218f60e2cad0a80928c4145d12f1bf49aebab5",
+      tokenName: "4d657368546f6b656e",
+    };
+
+    const interestDatum: InterestDatum = {
+      repayLoanAmount: BigInt(100),
+      repayLoanAsset: loanAsset,
+      repayInterestAmount: BigInt(100),
+      repayInterestAsset: interestAsset,
+      lenderPubKeyHash: accoun1PubKeyHash as string,
+    };
+
+    const datum = Data.to(interestDatum, InterestDatum);
+
+    const cancelLoanUTxO: UTxO = {
+      txHash:
+        "009e369a09d92ef324b361668978055d1d707941db2db670d79ea0f6f93a7f67",
+      outputIndex: 1,
+      assets: {
+        lovelace: 1861920n,
+        a1deebd26b685e6799218f60e2cad0a80928c4145d12f1bf49aebab54d657368546f6b656e:
+          100n,
+      },
+      address: interestScriptAddress,
+      datumHash: undefined,
+      datum: datum,
+      scriptRef: undefined,
+    };
+
+    const interestConfig: InterestConfig = {
+      interestUTxOs: [cancelLoanUTxO],
+      lenderPubKeyHash: account2PubKeyHash as string,
+    };
+
+    const tx = await interestTx(lucid, interestConfig);
+
+    expect(tx.type).toBe("error");
+  });
 });
--- test/repayLoan.test.ts
+++ test/repayLoan.test.ts
@@ -1,4 +1,4 @@
-import { expect, test, beforeEach } from "vitest";
+import { expect, test, beforeEach, describe } from "vitest";
 import { Data, Emulator, Lucid, UTxO } from "lucid-cardano";
 import {
   generateAccountSeedPhrase,
@@ -12,100 +12,375 @@ import {
 
 type LucidContext = {
   lucid: Lucid;
-  lender: any;
+  users: any;
   emulator: Emulator;
 };
 
 beforeEach<LucidContext>(async (context) => {
-  const lender = await generateAccountSeedPhrase({
-    lovelace: BigInt(100_000_000),
-  });
-  context.lender = lender;
-  context.emulator = new Emulator([lender]);
+  context.users = {
+    account1: await generateAccountSeedPhrase({
+      lovelace: BigInt(100_000_000),
+    }),
+    account2: await generateAccountSeedPhrase({
+      lovelace: BigInt(100_000_000),
+    }),
+  };
+
+  context.emulator = new Emulator([
+    context.users.account1,
+    context.users.account2,
+  ]);
   context.lucid = await Lucid.new(context.emulator);
 });
 
-test<LucidContext>("Can get interest payment", async ({
-  lucid,
-  lender,
-  emulator,
-}) => {
-  lucid.selectWalletFromSeed(lender.seedPhrase);
+describe("Repay Loan", () => {
+  test<LucidContext>("Can repay loan", async ({ lucid, users, emulator }) => {
+    lucid.selectWalletFromSeed(users.account1.seedPhrase);
 
-  const { collateralScriptAddress } = await getValidators();
+    const { collateralScriptAddress } = await getValidators();
 
-  const asset = {
-    policyId: "a1deebd26b685e6799218f60e2cad0a80928c4145d12f1bf49aebab5",
-    tokenName: "4d657368546f6b656e",
-  };
+    const asset = {
+      policyId: "a1deebd26b685e6799218f60e2cad0a80928c4145d12f1bf49aebab5",
+      tokenName: "4d657368546f6b656e",
+    };
 
-  const lenderPubKeyHash = lucid.utils.getAddressDetails(
-    await lucid.wallet.address()
-  ).paymentCredential?.hash;
+    const lenderPubKeyHash = lucid.utils.getAddressDetails(
+      await lucid.wallet.address()
+    ).paymentCredential?.hash;
 
-  const collateralAsset: AssetClassD = {
-    policyId: asset.policyId,
-    tokenName: asset.tokenName,
-  };
-
-  const interestAsset: AssetClassD = {
-    policyId: "",
-    tokenName: "",
-  };
-
-  const loanAsset: AssetClassD = {
-    policyId: asset.policyId,
-    tokenName: asset.tokenName,
-  };
-
-  const collateralDatum: CollateralDatum = {
-    loanAmount: BigInt(5),
-    loanAsset: loanAsset,
-    collateralAsset: collateralAsset,
-    interestAmount: BigInt(5000000),
-    interestAsset: interestAsset,
-    loanDuration: BigInt(10000000000),
-    lendTime: BigInt(emulator.now() - 100_000),
-    totalInterestAmount: BigInt(5000000),
-    totalLoanAmount: BigInt(5),
-    lenderPubKeyHash: lenderPubKeyHash as string,
-    borrowerPubKeyHash: lenderPubKeyHash as string,
-  };
-
-  const datum = Data.to(collateralDatum, CollateralDatum);
-
-  const collateralUTxO: UTxO = {
-    txHash: "009e369a09d92ef324b361668978055d1d707941db2db670d79ea0f6f93a7f67",
-    outputIndex: 1,
-    assets: {
-      lovelace: 1861920n,
-      a1deebd26b685e6799218f60e2cad0a80928c4145d12f1bf49aebab54d657368546f6b656e:
-        100n,
-    },
-    address: collateralScriptAddress,
-    datumHash: undefined,
-    datum: datum,
-    scriptRef: undefined,
-  };
-
-  const interestUTxOsInfo = getInterestInfoFromCollateral([collateralDatum]);
-
-  const loanConfig: RepayLoanConfig = {
-    interestAsset: {
-      policyId: "",
-      tokenName: "",
-    },
-    loanAsset: {
+    const collateralAsset: AssetClassD = {
       policyId: asset.policyId,
       tokenName: asset.tokenName,
-    },
-    collateralUTxOs: [collateralUTxO],
-    interestUTxOsInfo: interestUTxOsInfo,
-    now: emulator.now(),
-    borrowerPubKeyHash: lenderPubKeyHash as string,
-  };
+    };
 
-  const tx = await repayLoanTx(lucid, loanConfig);
+    const interestAsset: AssetClassD = {
+      policyId: "",
+      tokenName: "",
+    };
 
-  expect(tx.type).toBe("success");
+    const loanAsset: AssetClassD = {
+      policyId: asset.policyId,
+      tokenName: asset.tokenName,
+    };
+
+    const collateralDatum: CollateralDatum = {
+      loanAmount: BigInt(5),
+      loanAsset: loanAsset,
+      collateralAsset: collateralAsset,
+      interestAmount: BigInt(5000000),
+      interestAsset: interestAsset,
+      loanDuration: BigInt(10000000000),
+      lendTime: BigInt(emulator.now() - 100_000),
+      totalInterestAmount: BigInt(5000000),
+      totalLoanAmount: BigInt(5),
+      lenderPubKeyHash: lenderPubKeyHash as string,
+      borrowerPubKeyHash: lenderPubKeyHash as string,
+    };
+
+    const datum = Data.to(collateralDatum, CollateralDatum);
+
+    const collateralUTxO: UTxO = {
+      txHash:
+        "009e369a09d92ef324b361668978055d1d707941db2db670d79ea0f6f93a7f67",
+      outputIndex: 1,
+      assets: {
+        lovelace: 1861920n,
+        a1deebd26b685e6799218f60e2cad0a80928c4145d12f1bf49aebab54d657368546f6b656e:
+          100n,
+      },
+      address: collateralScriptAddress,
+      datumHash: undefined,
+      datum: datum,
+      scriptRef: undefined,
+    };
+
+    const interestUTxOsInfo = getInterestInfoFromCollateral([collateralDatum]);
+
+    const loanConfig: RepayLoanConfig = {
+      interestAsset: {
+        policyId: "",
+        tokenName: "",
+      },
+      loanAsset: {
+        policyId: asset.policyId,
+        tokenName: asset.tokenName,
+      },
+      collateralUTxOs: [collateralUTxO],
+      interestUTxOsInfo: interestUTxOsInfo,
+      now: emulator.now(),
+      borrowerPubKeyHash: lenderPubKeyHash as string,
+    };
+
+    const tx = await repayLoanTx(lucid, loanConfig);
+
+    expect(tx.type).toBe("success");
+  });
+
+  test<LucidContext>("Can not repay loan if expired", async ({
+    lucid,
+    users,
+    emulator,
+  }) => {
+    lucid.selectWalletFromSeed(users.account1.seedPhrase);
+
+    const { collateralScriptAddress } = await getValidators();
+
+    const asset = {
+      policyId: "a1deebd26b685e6799218f60e2cad0a80928c4145d12f1bf49aebab5",
+      tokenName: "4d657368546f6b656e",
+    };
+
+    const lenderPubKeyHash = lucid.utils.getAddressDetails(
+      await lucid.wallet.address()
+    ).paymentCredential?.hash;
+
+    const collateralAsset: AssetClassD = {
+      policyId: asset.policyId,
+      tokenName: asset.tokenName,
+    };
+
+    const interestAsset: AssetClassD = {
+      policyId: "",
+      tokenName: "",
+    };
+
+    const loanAsset: AssetClassD = {
+      policyId: asset.policyId,
+      tokenName: asset.tokenName,
+    };
+
+    const collateralDatum: CollateralDatum = {
+      loanAmount: BigInt(5),
+      loanAsset: loanAsset,
+      collateralAsset: collateralAsset,
+      interestAmount: BigInt(5000000),
+      interestAsset: interestAsset,
+      loanDuration: BigInt(1),
+      lendTime: BigInt(emulator.now() - 100_000),
+      totalInterestAmount: BigInt(5000000),
+      totalLoanAmount: BigInt(5),
+      lenderPubKeyHash: lenderPubKeyHash as string,
+      borrowerPubKeyHash: lenderPubKeyHash as string,
+    };
+
+    const datum = Data.to(collateralDatum, CollateralDatum);
+
+    const collateralUTxO: UTxO = {
+      txHash:
+        "009e369a09d92ef324b361668978055d1d707941db2db670d79ea0f6f93a7f67",
+      outputIndex: 1,
+      assets: {
+        lovelace: 1861920n,
+        a1deebd26b685e6799218f60e2cad0a80928c4145d12f1bf49aebab54d657368546f6b656e:
+          100n,
+      },
+      address: collateralScriptAddress,
+      datumHash: undefined,
+      datum: datum,
+      scriptRef: undefined,
+    };
+
+    const interestUTxOsInfo = getInterestInfoFromCollateral([collateralDatum]);
+
+    const loanConfig: RepayLoanConfig = {
+      interestAsset: {
+        policyId: "",
+        tokenName: "",
+      },
+      loanAsset: {
+        policyId: asset.policyId,
+        tokenName: asset.tokenName,
+      },
+      collateralUTxOs: [collateralUTxO],
+      interestUTxOsInfo: interestUTxOsInfo,
+      now: emulator.now(),
+      borrowerPubKeyHash: lenderPubKeyHash as string,
+    };
+
+    const tx = await repayLoanTx(lucid, loanConfig);
+
+    expect(tx.type).toBe("error");
+  });
+
+  test<LucidContext>("Can not repay loan if output interest info doesnt match", async ({
+    lucid,
+    users,
+    emulator,
+  }) => {
+    lucid.selectWalletFromSeed(users.account1.seedPhrase);
+
+    const { collateralScriptAddress } = await getValidators();
+
+    const asset = {
+      policyId: "a1deebd26b685e6799218f60e2cad0a80928c4145d12f1bf49aebab5",
+      tokenName: "4d657368546f6b656e",
+    };
+
+    const lenderPubKeyHash = lucid.utils.getAddressDetails(
+      await lucid.wallet.address()
+    ).paymentCredential?.hash;
+
+    const collateralAsset: AssetClassD = {
+      policyId: asset.policyId,
+      tokenName: asset.tokenName,
+    };
+
+    const interestAsset: AssetClassD = {
+      policyId: "",
+      tokenName: "",
+    };
+
+    const loanAsset: AssetClassD = {
+      policyId: asset.policyId,
+      tokenName: asset.tokenName,
+    };
+
+    const collateralDatum: CollateralDatum = {
+      loanAmount: BigInt(5),
+      loanAsset: loanAsset,
+      collateralAsset: collateralAsset,
+      interestAmount: BigInt(5000000),
+      interestAsset: interestAsset,
+      loanDuration: BigInt(10000000000),
+      lendTime: BigInt(emulator.now() - 100_000),
+      totalInterestAmount: BigInt(5000000),
+      totalLoanAmount: BigInt(5),
+      lenderPubKeyHash: lenderPubKeyHash as string,
+      borrowerPubKeyHash: lenderPubKeyHash as string,
+    };
+
+    const datum = Data.to(collateralDatum, CollateralDatum);
+
+    const collateralUTxO: UTxO = {
+      txHash:
+        "009e369a09d92ef324b361668978055d1d707941db2db670d79ea0f6f93a7f67",
+      outputIndex: 1,
+      assets: {
+        lovelace: 1861920n,
+        a1deebd26b685e6799218f60e2cad0a80928c4145d12f1bf49aebab54d657368546f6b656e:
+          100n,
+      },
+      address: collateralScriptAddress,
+      datumHash: undefined,
+      datum: datum,
+      scriptRef: undefined,
+    };
+
+    const interestUTxOsInfo = getInterestInfoFromCollateral([collateralDatum]);
+
+    const loanConfig: RepayLoanConfig = {
+      interestAsset: {
+        policyId: "",
+        tokenName: "",
+      },
+      loanAsset: {
+        policyId: asset.policyId,
+        tokenName: asset.tokenName,
+      },
+      collateralUTxOs: [collateralUTxO],
+      interestUTxOsInfo: [
+        {
+          repayInterestAmount: 5000000,
+          repayLoanAmount: 4,
+          lenderPubKeyHash: lenderPubKeyHash as string,
+        },
+      ],
+      now: emulator.now(),
+      borrowerPubKeyHash: lenderPubKeyHash as string,
+    };
+
+    const tx = await repayLoanTx(lucid, loanConfig);
+
+    expect(tx.type).toBe("error");
+  });
+
+  test<LucidContext>("Can not repay loan if not original borrower", async ({
+    lucid,
+    users,
+    emulator,
+  }) => {
+    lucid.selectWalletFromSeed(users.account1.seedPhrase);
+
+    const { collateralScriptAddress } = await getValidators();
+
+    const asset = {
+      policyId: "a1deebd26b685e6799218f60e2cad0a80928c4145d12f1bf49aebab5",
+      tokenName: "4d657368546f6b656e",
+    };
+
+    const account1PubKeyHash = lucid.utils.getAddressDetails(
+      await lucid.wallet.address()
+    ).paymentCredential?.hash;
+
+    const account2PubKeyHash = lucid.utils.getAddressDetails(
+      await users.account2.address
+    ).paymentCredential?.hash;
+
+    const collateralAsset: AssetClassD = {
+      policyId: asset.policyId,
+      tokenName: asset.tokenName,
+    };
+
+    const interestAsset: AssetClassD = {
+      policyId: "",
+      tokenName: "",
+    };
+
+    const loanAsset: AssetClassD = {
+      policyId: asset.policyId,
+      tokenName: asset.tokenName,
+    };
+
+    const collateralDatum: CollateralDatum = {
+      loanAmount: BigInt(5),
+      loanAsset: loanAsset,
+      collateralAsset: collateralAsset,
+      interestAmount: BigInt(5000000),
+      interestAsset: interestAsset,
+      loanDuration: BigInt(10000000000),
+      lendTime: BigInt(emulator.now() - 100_000),
+      totalInterestAmount: BigInt(5000000),
+      totalLoanAmount: BigInt(5),
+      lenderPubKeyHash: account1PubKeyHash as string,
+      borrowerPubKeyHash: account2PubKeyHash as string,
+    };
+
+    const datum = Data.to(collateralDatum, CollateralDatum);
+
+    const collateralUTxO: UTxO = {
+      txHash:
+        "009e369a09d92ef324b361668978055d1d707941db2db670d79ea0f6f93a7f67",
+      outputIndex: 1,
+      assets: {
+        lovelace: 1861920n,
+        a1deebd26b685e6799218f60e2cad0a80928c4145d12f1bf49aebab54d657368546f6b656e:
+          100n,
+      },
+      address: collateralScriptAddress,
+      datumHash: undefined,
+      datum: datum,
+      scriptRef: undefined,
+    };
+
+    const interestUTxOsInfo = getInterestInfoFromCollateral([collateralDatum]);
+
+    const loanConfig: RepayLoanConfig = {
+      interestAsset: {
+        policyId: "",
+        tokenName: "",
+      },
+      loanAsset: {
+        policyId: asset.policyId,
+        tokenName: asset.tokenName,
+      },
+      collateralUTxOs: [collateralUTxO],
+      interestUTxOsInfo: interestUTxOsInfo,
+      now: emulator.now(),
+      borrowerPubKeyHash: account1PubKeyHash as string,
+    };
+
+    const tx = await repayLoanTx(lucid, loanConfig);
+
+    expect(tx.type).toBe("error");
+  });
 });
